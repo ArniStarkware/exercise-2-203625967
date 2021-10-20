bb3dfb0e936c75b5d7595ff0c9d31c9effd3bbdf
ArniStarkware <arnon@starkware.co>
2021-10-20 17:21:52 +0300

    submitting exercise 2

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
-    python -m virtualenv .env --prompt "[exercise 2] "
+    python3 -m virtualenv .env --prompt "[exercise 2] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     pip install -U pip
     pip install -r requirements.txt
