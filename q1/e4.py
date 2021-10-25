ce58f161cb4c2b526d3b53170a3543ced46069e0
ArniStarkware <arnon@starkware.co>
2021-10-25 09:40:28 +0300

    q1_e4

--- q1/e4.py
+++ q1/e4.py
@@ -1,2 +1,17 @@
+import time
+
 def time_execution(f):
-    pass # TODO
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        try:
+            out = f(*args,**kwargs)
+            end = time.time()
+            elapsed = end - start
+            print(f"{f.__name__} took '{elapsed:0.2f}' seconds to execute")
+            return out
+        except:
+            end = time.time()
+            elapsed = end - start
+            print(f"{f.__name__} took '{elapsed:0.2f}' seconds to execute")
+            raise
+    return wrapper
