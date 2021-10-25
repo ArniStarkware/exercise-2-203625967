b2eef0a65cc5ccc721a2f411d9a0b666da873556
ArniStarkware <arnon@starkware.co>
2021-10-25 15:42:09 +0300

    q1 e7 - after much delib

--- q1/e7.py
+++ q1/e7.py
@@ -1,2 +1,16 @@
+import inspect
+
 def validate_types(**types):
-    pass # TODO
+    # TODO - I did not understand the commant about inspect.getcallargs
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            callargs = inspect.getcallargs(f, *args, **kwargs)
+            for key,value in callargs.items():
+                if not type(value) == types[key]:
+                    raise ValueError
+            out = f(*args,**kwargs)
+            if not type(out) == types['return_value']:
+                raise ValueError
+            return out
+        return wrapper
+    return decorator
